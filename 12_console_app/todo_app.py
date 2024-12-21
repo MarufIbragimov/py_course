361d71f358537209b96c4f665a32f4d429c69b2b
maruf ibragimov <ibragimov.maruf@gmail.com>
2024-12-22 00:00:20 +0500

    add cancel option to `add_task` function

--- 12_console_app/todo_app.py
+++ 12_console_app/todo_app.py
@@ -3,9 +3,9 @@
 TXT_MAX_LENGTH: int = 50
 FILLER_SYMBOL: str = '*'
 tasks: dict[int, str] = {}
+info_txt = "Выберите нужную команду введя её номер: "
 
-
-MAIN_MENU: dict[int, str] = {
+main_menu: dict[int, str] = {
     1: 'Добавить новую задачу',
     2: 'Вывести список задач',
     3: 'Редактировать задачу',
@@ -13,7 +13,7 @@ MAIN_MENU: dict[int, str] = {
     0: 'Выход'
 }
 
-TASK_DETAILS: dict[int, dict[str, str]] = {
+task_details: dict[int, dict[str, str]] = {
     1: {'option': 'task_id', 'caption': 'id задачи'},
     2: {'option': 'is_done', 'caption': 'статус задачи'},
     3: {'option': 'title', 'caption': 'название задачи'},
@@ -21,6 +21,50 @@ TASK_DETAILS: dict[int, dict[str, str]] = {
     5: {'option': 'deadline', 'caption': 'дедлайн задачи'},
     6: {'option': 'priority', 'caption': 'приоритет задачи'},
 }
+
+task_addition_submenu: dict[int, str] = {
+    1: 'Добавить ещё одну задачу',
+    2: 'Вернуться в главное меню'
+}
+
+menus: dict[int, dict[str, dict[int, str]]] = {
+    1: {'caption': 'ГЛАВНОЕ МЕНЮ', 'menu': main_menu},
+    2: {'caption': 'МЕНЮ', 'menu': task_addition_submenu}
+}
+
+
+def show_menu(menu_id: int):
+    menu_caption = menus[menu_id]['caption']
+    menu_caption = f"  {menu_caption}  "
+    menu = menus[menu_id]['menu']
+
+    print(f"\n{menu_caption.center(TXT_MAX_LENGTH, FILLER_SYMBOL)}")
+    for key, value in menu.items():
+        print(f"{key}. {value}")
+    print('\n')
+    
+
+def validate(user_input, menu_id: int) -> bool:
+    user_input = user_input.strip()
+    menu = menus[menu_id]['menu']
+
+    is_valid = user_input.isnumeric() and int(user_input) in menu.keys()
+    return is_valid
+    
+
+def select_action(menu_id: int) -> int:
+    show_menu(menu_id)
+
+    valid_choice = False
+    while not valid_choice:
+        selected_option = input(info_txt)
+        valid_choice = validate(selected_option, menu_id)
+    
+        if not valid_choice:
+            warning_txt = "Вы ввели неверную команду. Попробуйте ещё раз."
+            print(warning_txt, end='\n\n')
+    
+    return int(selected_option)
 
 
 def assign_id() -> int:
@@ -37,57 +81,33 @@ def assign_id() -> int:
 
 def add_task():
     
-    print(f"{'Добавление задачи'.center(TXT_MAX_LENGTH, FILLER_SYMBOL)}")
+    print(f"\n{' ДОБАВЛЕНИЕ ЗАДАЧИ (для отмены введите 0) '.center(TXT_MAX_LENGTH, FILLER_SYMBOL)}")
 
     new_task = dict()
     task_id = assign_id()
     new_task['task_id'] = task_id
     new_task['is_done'] = False
 
-    task_details_count = len(TASK_DETAILS) 
+    task_details_count = len(task_details) 
     for ix in range(3, task_details_count):
-        detail = TASK_DETAILS[ix]
+        detail = task_details[ix]
         option = detail['option']
         caption = detail['caption']
 
         value = input(f"Введите {caption}: ")
+        if value.strip() == '0':
+            print("\nДЕЙСТВИЕ БЫЛО ОТМЕНЕНО", end='\n\n')
+            return
+        
         new_task[option] = value
 
     tasks[task_id] = new_task
 
-    print(f"--------------Задача {task_id} успешно  добавлена--------------")
+    print(f"\nЗАДАЧА {task_id} УСПЕШНО ДОБАВЛЕНА!", end='\n\n')
 
-
-
-def show_menu():
-    print(f"{'Главное меню'.center(TXT_MAX_LENGTH, FILLER_SYMBOL)}")
-    print(f"1. {MAIN_MENU[1]}")
-    print(f"2. {MAIN_MENU[2]}")
-    print(f"3. {MAIN_MENU[3]}")
-    print(f"4. {MAIN_MENU[4]}")
-    print(f"0. {MAIN_MENU[0]}", end="\n\n") 
-    
-
-def validate(user_input) -> bool:
-    user_input = user_input.strip()
-    is_valid = user_input.isnumeric() and int(user_input) in MAIN_MENU.keys()
-    return is_valid
-    
-
-def select_action() -> int:
-    info_txt = "Выберите нужную команду введя её номер: "
-    show_menu()
-
-    valid_choice = False
-    while not valid_choice:
-        selected_option = input(info_txt)
-        valid_choice = validate(selected_option)
-    
-        if not valid_choice:
-            warning_txt = "Вы ввели неверную команду. Попробуйте ещё раз."
-            print(warning_txt, end='\n\n')
-    
-    return int(selected_option)
+    user_choice = select_action(2)
+    if user_choice == 1:
+        add_task()
 
 
 def main():
@@ -95,7 +115,7 @@ def main():
     print("\nДобро пожаловать в Todo-List-App!", end='\n\n')
 
     while True:
-        user_choice = select_action()
+        user_choice = select_action(1)
         
         match user_choice:
             case 0:
@@ -107,6 +127,8 @@ def main():
 
         
         # options[user_choice]
+
+
 
 
 main()
